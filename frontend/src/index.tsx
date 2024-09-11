bf95fb2c94784f6793a430b403ae62a7fc543133
SneakyBrian <SneakyBrian@users.noreply.github.com>
2024-09-11 19:50:18 +0100

    converted to webpack

---
+++ frontend/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Torrent Search</h1>
+      {/* Add your components here */}
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
