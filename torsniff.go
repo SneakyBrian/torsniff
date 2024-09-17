661ccb1f0c30ae5dc3876aef688710df20f450f7
SneakyBrian <SneakyBrian@users.noreply.github.com>
2024-09-17 23:00:26 +0100

    refactor: clean up import statements and improve code formatting in torsniff.go

--- torsniff.go
+++ torsniff.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"bytes"
+	"encoding/binary"
 	"fmt"
 	"io"
 	"log"
-	"encoding/binary"
 	"math"
-	"net/url"
 	"math/rand"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"os/signal"
 	"strconv"
@@ -18,7 +18,6 @@ import (
 	"sync"
 	"syscall"
 	"time"
-	"math/rand"
 
 	"github.com/marksamman/bencode"
 	"github.com/spf13/cobra"
@@ -30,185 +29,185 @@ type tfile struct {
 }
 
 func scrapeHTTPTracker(trackerURL string, infohash []byte) (int, int, error) {
-    // Construct the scrape URL
-    scrapeURL := strings.Replace(trackerURL, "/announce", "/scrape", 1)
-    u, err := url.Parse(scrapeURL)
-    if err != nil {
-        return 0, 0, err
-    }
+	// Construct the scrape URL
+	scrapeURL := strings.Replace(trackerURL, "/announce", "/scrape", 1)
+	u, err := url.Parse(scrapeURL)
+	if err != nil {
+		return 0, 0, err
+	}
 
-    // Add the infohash to the query parameters
-    q := u.Query()
-    q.Set("info_hash", string(infohash))
-    u.RawQuery = q.Encode()
+	// Add the infohash to the query parameters
+	q := u.Query()
+	q.Set("info_hash", string(infohash))
+	u.RawQuery = q.Encode()
 
-    // Send the HTTP request
-    resp, err := http.Get(u.String())
-    if err != nil {
-        return 0, 0, err
-    }
-    defer resp.Body.Close()
+	// Send the HTTP request
+	resp, err := http.Get(u.String())
+	if err != nil {
+		return 0, 0, err
+	}
+	defer resp.Body.Close()
 
-    if resp.StatusCode != http.StatusOK {
-        return 0, 0, fmt.Errorf("failed to scrape tracker: %s", resp.Status)
-    }
+	if resp.StatusCode != http.StatusOK {
+		return 0, 0, fmt.Errorf("failed to scrape tracker: %s", resp.Status)
+	}
 
-    // Parse the response
-    body, err := io.ReadAll(resp.Body)
-    if err != nil {
-        return 0, 0, err
-    }
+	// Parse the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return 0, 0, err
+	}
 
-    data, err := bencode.Decode(bytes.NewBuffer(body))
-    if err != nil {
-        return 0, 0, err
-    }
+	data, err := bencode.Decode(bytes.NewBuffer(body))
+	if err != nil {
+		return 0, 0, err
+	}
 
-    // Extract seeders and leechers
-    files, ok := data["files"].(map[string]interface{})
-    if !ok {
-        return 0, 0, fmt.Errorf("invalid scrape response")
-    }
+	// Extract seeders and leechers
+	files, ok := data["files"].(map[string]interface{})
+	if !ok {
+		return 0, 0, fmt.Errorf("invalid scrape response")
+	}
 
-    stats, ok := files[string(infohash)].(map[string]interface{})
-    if !ok {
-        return 0, 0, fmt.Errorf("infohash not found in scrape response")
-    }
+	stats, ok := files[string(infohash)].(map[string]interface{})
+	if !ok {
+		return 0, 0, fmt.Errorf("infohash not found in scrape response")
+	}
 
-    seeders, _ := stats["complete"].(int)
-    leechers, _ := stats["incomplete"].(int)
+	seeders, _ := stats["complete"].(int)
+	leechers, _ := stats["incomplete"].(int)
 
-    return seeders, leechers, nil
+	return seeders, leechers, nil
 }
 
 const (
-	protocolID   = 0x41727101980 // Protocol ID for UDP trackers
+	protocolID    = 0x41727101980 // Protocol ID for UDP trackers
 	actionConnect = 0
 	actionScrape  = 2
 )
 
 func scrapeUDPTracker(trackerURL string, infohash []byte) (int, int, error) {
-    // Resolve the UDP address
-    addr, err := net.ResolveUDPAddr("udp", trackerURL)
-    if err != nil {
-        return 0, 0, err
-    }
+	// Resolve the UDP address
+	addr, err := net.ResolveUDPAddr("udp", trackerURL)
+	if err != nil {
+		return 0, 0, err
+	}
 
-    // Create a UDP connection
-    conn, err := net.DialUDP("udp", nil, addr)
-    if err != nil {
-        return 0, 0, err
-    }
-    defer conn.Close()
+	// Create a UDP connection
+	conn, err := net.DialUDP("udp", nil, addr)
+	if err != nil {
+		return 0, 0, err
+	}
+	defer conn.Close()
 
-    // Set a timeout for the connection
-    conn.SetDeadline(time.Now().Add(15 * time.Second))
+	// Set a timeout for the connection
+	conn.SetDeadline(time.Now().Add(15 * time.Second))
 
-    // Send a connect request
-    connID, err := sendConnectRequest(conn)
-    if err != nil {
-        return 0, 0, err
-    }
+	// Send a connect request
+	connID, err := sendConnectRequest(conn)
+	if err != nil {
+		return 0, 0, err
+	}
 
-    // Send a scrape request
-    seeds, leechers, err := sendScrapeRequest(conn, connID, infohash)
-    if err != nil {
-        return 0, 0, err
-    }
+	// Send a scrape request
+	seeds, leechers, err := sendScrapeRequest(conn, connID, infohash)
+	if err != nil {
+		return 0, 0, err
+	}
 
-    return seeds, leechers, nil
+	return seeds, leechers, nil
 }
 
 func sendConnectRequest(conn *net.UDPConn) (int64, error) {
-    // Create a connect request packet
-    buf := new(bytes.Buffer)
-    binary.Write(buf, binary.BigEndian, protocolID)
-    binary.Write(buf, binary.BigEndian, int32(actionConnect))
-    binary.Write(buf, binary.BigEndian, int32(rand.Int31()))
+	// Create a connect request packet
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, protocolID)
+	binary.Write(buf, binary.BigEndian, int32(actionConnect))
+	binary.Write(buf, binary.BigEndian, int32(rand.Int31()))
 
-    // Send the packet
-    _, err := conn.Write(buf.Bytes())
-    if err != nil {
-        return 0, err
-    }
+	// Send the packet
+	_, err := conn.Write(buf.Bytes())
+	if err != nil {
+		return 0, err
+	}
 
-    // Read the response
-    resp := make([]byte, 16)
-    _, err = conn.Read(resp)
-    if err != nil {
-        return 0, err
-    }
+	// Read the response
+	resp := make([]byte, 16)
+	_, err = conn.Read(resp)
+	if err != nil {
+		return 0, err
+	}
 
-    // Parse the response
-    if binary.BigEndian.Uint32(resp[0:4]) != actionConnect {
-        return 0, fmt.Errorf("invalid connect response")
-    }
+	// Parse the response
+	if binary.BigEndian.Uint32(resp[0:4]) != actionConnect {
+		return 0, fmt.Errorf("invalid connect response")
+	}
 
-    return int64(binary.BigEndian.Uint64(resp[8:16])), nil
+	return int64(binary.BigEndian.Uint64(resp[8:16])), nil
 }
 
 func sendScrapeRequest(conn *net.UDPConn, connID int64, infohash []byte) (int, int, error) {
-    // Create a scrape request packet
-    buf := new(bytes.Buffer)
-    binary.Write(buf, binary.BigEndian, connID)
-    binary.Write(buf, binary.BigEndian, int32(actionScrape))
-    binary.Write(buf, binary.BigEndian, int32(rand.Int31()))
-    buf.Write(infohash)
+	// Create a scrape request packet
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.BigEndian, connID)
+	binary.Write(buf, binary.BigEndian, int32(actionScrape))
+	binary.Write(buf, binary.BigEndian, int32(rand.Int31()))
+	buf.Write(infohash)
 
-    // Send the packet
-    _, err := conn.Write(buf.Bytes())
-    if err != nil {
-        return 0, 0, err
-    }
+	// Send the packet
+	_, err := conn.Write(buf.Bytes())
+	if err != nil {
+		return 0, 0, err
+	}
 
-    // Read the response
-    resp := make([]byte, 20)
-    _, err = conn.Read(resp)
-    if err != nil {
-        return 0, 0, err
-    }
+	// Read the response
+	resp := make([]byte, 20)
+	_, err = conn.Read(resp)
+	if err != nil {
+		return 0, 0, err
+	}
 
-    // Parse the response
-    if binary.BigEndian.Uint32(resp[0:4]) != actionScrape {
-        return 0, 0, fmt.Errorf("invalid scrape response")
-    }
+	// Parse the response
+	if binary.BigEndian.Uint32(resp[0:4]) != actionScrape {
+		return 0, 0, fmt.Errorf("invalid scrape response")
+	}
 
-    seeds := int(binary.BigEndian.Uint32(resp[8:12]))
-    leechers := int(binary.BigEndian.Uint32(resp[12:16]))
+	seeds := int(binary.BigEndian.Uint32(resp[8:12]))
+	leechers := int(binary.BigEndian.Uint32(resp[12:16]))
 
-    return seeds, leechers, nil
+	return seeds, leechers, nil
 }
 
 func scrapeTrackers(infohash []byte) (int, int) {
-    var maxSeeds, maxLeechers int
+	var maxSeeds, maxLeechers int
 
-    trackersMutex.RLock()
-    defer trackersMutex.RUnlock()
+	trackersMutex.RLock()
+	defer trackersMutex.RUnlock()
 
-    for _, tracker := range trackersList {
-        var seeds, leechers int
-        var err error
+	for _, tracker := range trackersList {
+		var seeds, leechers int
+		var err error
 
-        if strings.HasPrefix(tracker, "http") {
-            seeds, leechers, err = scrapeHTTPTracker(tracker, infohash)
-        } else if strings.HasPrefix(tracker, "udp") {
-            seeds, leechers, err = scrapeUDPTracker(tracker, infohash)
-        }
+		if strings.HasPrefix(tracker, "http") {
+			seeds, leechers, err = scrapeHTTPTracker(tracker, infohash)
+		} else if strings.HasPrefix(tracker, "udp") {
+			seeds, leechers, err = scrapeUDPTracker(tracker, infohash)
+		}
 
-        if err != nil {
-            log.Printf("error scraping tracker %s: %v", tracker, err)
-            continue
-        }
+		if err != nil {
+			log.Printf("error scraping tracker %s: %v", tracker, err)
+			continue
+		}
 
-        if seeds > maxSeeds {
-            maxSeeds = seeds
-        }
-        if leechers > maxLeechers {
-            maxLeechers = leechers
-        }
-    }
+		if seeds > maxSeeds {
+			maxSeeds = seeds
+		}
+		if leechers > maxLeechers {
+			maxLeechers = leechers
+		}
+	}
 
-    return maxSeeds, maxLeechers
+	return maxSeeds, maxLeechers
 }
 
 func (t *tfile) String() string {
